7e13d82f0059e176c6f03258d437ee76ee31526d
fractalcounty <chip@fractalcounty.com>
2024-11-08 08:04:05 -0600

    added music to index

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,12 @@
 /// <reference path="../.astro/types.d.ts" />
+/// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly LASTFM_API_KEY: string
+  readonly SPOTIFY_CLIENT_ID: string
+  readonly SPOTIFY_CLIENT_SECRET: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
